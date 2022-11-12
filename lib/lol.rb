57e829acaa5a9b4960617221a00fdd64494261fd
Josh Teperman <Josh.teperman@weployapp.com>
2022-11-12 21:29:24 +1100

    add basic controller & routing

--- lib/lol.rb
+++ lib/lol.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 require "lol/version"
+require "lol/routing"
+require "lol/controller"
 
 module Lol
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["Hello from lol-framework"]]
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [
+        200,
+        {'Content-Type' => 'text/html'},
+        [text]
+      ]
     end
   end
 end
