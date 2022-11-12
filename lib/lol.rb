438f7dd3a027741cdaafbe5492f44258355eb431
Josh Teperman <Josh.teperman@weployapp.com>
2022-11-12 23:07:13 +1100

    hack solution to browser trying to fetch /favicon.ico

--- lib/lol.rb
+++ lib/lol.rb
@@ -7,6 +7,14 @@ require "lol/controller"
 module Lol
   class Application
     def call(env)
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [
+          400,
+          { "Content-Type" => "text/html" },
+          []
+        ]
+      end
+
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(action)
